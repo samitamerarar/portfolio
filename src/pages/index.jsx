d113a31697087ecff3ed00316b4e1f603da1e52d
samitamerarar <sami.t.arar@hotmail.com>
2025-05-20 10:51:46 -0400

    Updated Icons location on Homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -318,22 +318,22 @@ export default function Home({ articles }) {
       </Head>
       <Container className="relative mt-9">
         <div className="max-w-2xl">
-          <div className="flex items-center justify-between">
-            <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              {t('container_div1_h1', { name: 'Sami' })}
-            </h1>
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            {t('container_div1_h1', { name: 'Sami' })}
+          </h1>
+          <p className="mt-6 mb-2 text-base text-zinc-600 dark:text-zinc-400">
+            {t('head_content')}
+          </p>
+          <div className="flex flex-wrap items-center justify-between gap-4">
+            <span className="text-sm font-medium text-zinc-700 dark:text-zinc-200">
+              {t('container_div1_span1')}
+            </span>
             <div className="flex gap-3">
               <Image src={logoSF} alt="sf" className="h-9 w-auto md:h-12" />
               <Image src={logoAWS} alt="aws" className="h-8 w-auto md:h-10" />
               <Image src={logoNS} alt="ns" className="h-10 w-auto md:h-14" />
             </div>
           </div>
-          <p className="mt-6 mb-2 text-base text-zinc-600 dark:text-zinc-400">
-            {t('head_content')}
-          </p>
-          <span className="text-sm font-medium text-zinc-700 dark:text-zinc-200">
-            {t('container_div1_span1')}
-          </span>
           <div className="mt-4 flex gap-6">
             <SocialLink
               href="https://github.com/samitamerarar"
